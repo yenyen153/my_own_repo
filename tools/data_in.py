a8cece09d8c122b887ec210687779c03e7942971
呂心妍 <hsinyenlu@MacBook-Air.local>
2025-02-25 17:51:43 +0800

    rename the modules

--- tools/data_in.py
+++ tools/data_in.py
@@ -1,28 +1,29 @@
 from sqlalchemy.orm import sessionmaker
 from tools.pydantic_databases import *
-import re
+
 
 Session = sessionmaker(bind=engine)
 
-def DataCheck(Session,**ptt_post):
+## name way change
+def data_check(Session,**ptt_post): # todo: change name to define style
     with Session() as session:
         author = session.query(AuthorTable).filter_by(name=ptt_post['author_name']).first()
         board = session.query(BoardTable).filter_by(board=ptt_post['board_id']).first()
 
 
         if not author:
-            a = ptt_post['author_name']
-            session.add(AuthorTable(**{'name':a,
-                                        'posts_url':f"https://www.ptt.cc/bbs/{ptt_post['board_id']}/search?q=author%3A{a}"}))
+
+            session.add(AuthorTable(**{'name':ptt_post['author_name'],
+                                        'posts_url':f"https://www.ptt.cc/bbs/{ptt_post['board_id']}/search?q=author%3A{ptt_post['author_name']}"}))
 
         if not board:
-            b = ptt_post['board_id']
-            session.add(BoardTable(**{'board':b,
-                                      'url': f'https://www.ptt.cc/bbs/{b}/index.html', }))
+
+            session.add(BoardTable(**{'board':ptt_post['board_id'],
+                                      'url': f"https://www.ptt.cc/bbs/{ptt_post['board_id']}/index.html" }))
         session.commit()
         session.close()
 
-def DataIn(Session,**ptt_post):
+def data_in(Session,**ptt_post): # todo: change name to define style
     with Session() as session:
         post_link = session.query(PttPostsTable).filter_by(link=ptt_post['link']).first()
         author = session.query(AuthorTable).filter_by(name=ptt_post['author_name']).first()
@@ -41,21 +42,26 @@ def DataIn(Session,**ptt_post):
         session.commit()
         session.close()
 
-def LogIn(Session,Crawlerlog):
-    with Session() as session:
-        with open(Crawlerlog, "r", encoding='utf-8') as log_file:
-            logs = log_file.readlines()
-            for log in logs:
-                match = re.match(r"([\d-]+ [\d:,]+) (.+)", log)
 
-                if match:
-                    time = match.group(1)
-                    message = match.group(2)
-                    time_log = {'time':time, 'message':message}
-                    session.add(CrawlerLog(**time_log))
 
-                session.commit()
-                session.close()
+
+
+
+# def LogIn(Session,Crawlerlog):
+#     with Session() as session:
+#         with open(Crawlerlog, "r", encoding='utf-8') as log_file:
+#             logs = log_file.readlines()
+#             for log in logs:
+#                 match = re.match(r"([\d-]+ [\d:,]+) (.+)", log)
+#
+#                 if match:
+#                     time = match.group(1)
+#                     message = match.group(2)
+#                     time_log = {'time':time, 'message':message}
+#                     session.add(CrawlerLog(**time_log))
+#
+#                 session.commit()
+#                 session.close()
 
 # def CrawlerLogIn(crawler_log):
 #     with Session() as session:
