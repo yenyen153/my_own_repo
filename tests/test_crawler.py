24f1e9747e913117aa9d63a7749f7eab966460d2
yenyen153 <live871008@gmail.com>
2025-02-25 11:50:44 +0800

    test crawler &test data insert

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -1,33 +1,27 @@
 import pytest
 import requests
-from unittest.mock import patch, MagicMock
-from bs4 import BeautifulSoup
+from unittest.mock import MagicMock
 from tools.crawler import fetch_posts
+
+
+
 
 MOCK_HTML = """
 <html>
     <body>
-		<div class="r-ent">
-			<div class="nrec"><span class="hl f3">23</span></div>
-			<div class="title">
-				<a href="/bbs/NBA/M.1740027377.A.CDD.html">[新聞] 林書豪6年來首踏NBA球場！為何曾拒絕參加</a>
-			</div>
-			<div class="meta">
-				<div class="author">access4096</div>
-				<div class="article-menu">
-					<div class="trigger">&#x22ef;</div>
-					<div class="dropdown">
-						<div class="item"><a href="/bbs/NBA/search?q=thread%3A%5B%E6%96%B0%E8%81%9E%5D&#43;%E6%9E%97%E6%9B%B8%E8%B1%AA6%E5%B9%B4%E4%BE%86%E9%A6%96%E8%B8%8FNBA%E7%90%83%E5%A0%B4%EF%BC%81%E7%82%BA%E4%BD%95%E6%9B%BE%E6%8B%92%E7%B5%95%E5%8F%83%E5%8A%A0">搜尋同標題文章</a></div>
-						<div class="item"><a href="/bbs/NBA/search?q=author%3Aaccess4096">搜尋看板內 access4096 的文章</a></div>
-					</div>
-				</div>
-				<div class="date">2/20</div>
-				<div class="mark"></div>
-			</div>
-		</div>
+        <div class="r-ent">
+            <div class="title">
+                <a href="/bbs/NBA/M.1740027377.A.CDD.html">[新聞] 林書豪6年來首踏NBA球場！為何曾拒絕參加</a>
+            </div>
+            <div class="meta">
+                <div class="author">access4096</div>
+                <div class="date">2/20</div>
+            </div>
+        </div>
     </body>
 </html>
 """
+
 
 @pytest.fixture
 def mock_requests_get(mocker):
@@ -35,6 +29,7 @@ def mock_requests_get(mocker):
     mock_response = MagicMock()
     mock_response.text = MOCK_HTML
     mocker.patch("requests.Session.get", return_value=mock_response)
+
 
 def test_fetch_posts(mock_requests_get):
 
@@ -48,11 +43,9 @@ def test_fetch_posts(mock_requests_get):
     assert posts[0]["date"] == "2/20"
 
 
+def test_fetch_posts_real():
 
-@pytest.fixture
-def test_fetch_posts_work():
-
-    posts = fetch_posts('NBA')
+    posts = fetch_posts("NBA")
 
     assert requests.get("https://www.ptt.cc/bbs/NBA/index.html").status_code == 200
     assert posts[0]['board_id'] == 'NBA'
@@ -61,4 +54,3 @@ def test_fetch_posts_work():
     assert "date" in posts[0]
     assert "link" in posts[0]
     assert len(posts) > 1
-
