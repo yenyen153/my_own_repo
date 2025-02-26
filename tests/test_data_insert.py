d4490ca51e749de2acdbf06a42b20a4324a46f79
呂心妍 <hsinyenlu@MacBook-Air.local>
2025-02-26 11:51:15 +0800

    update

--- tests/test_data_insert.py
+++ tests/test_data_insert.py
@@ -21,7 +21,7 @@ def test_data_insert():
         "board_name": "NBA",
         "title": "[新聞] 林書豪6年來首踏NBA球場！為何曾拒絕參加",
         "link": "https://www.ptt.cc/bbs/NBA/M.1740027377.A.CDD.html",
-        "author_id": "Test_Author_ID",
+        "author_ptt_id": "Test_Author_ID",
         "author_nickname": "Test_Author_Nickname",
         "date": "2025/02/26 14:38:12",
         "content": "這是測試內容"
@@ -40,8 +40,7 @@ def test_data_insert():
         author = session.query(AuthorTable).first()
 
         assert post.title == test_post["title"]
-        assert post.author_id == test_post["author_id"]
         assert post.board_id is not None
         assert board.id is not None
         assert author.author_nickname == test_post["author_nickname"]
-        assert author.author_id == test_post["author_id"]
+        assert author.author_ptt_id == test_post["author_ptt_id"]
