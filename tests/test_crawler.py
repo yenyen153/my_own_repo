33fcaf49826de9dfce340710e29386fee8eacbfa
呂心妍 <hsinyenlu@MacBook-Air.local>
2025-02-26 11:29:32 +0800

    commit first but not done

--- tests/test_crawler.py
+++ tests/test_crawler.py
@@ -12,7 +12,7 @@ MOCK_HTML = """
             </div>
             <div class="meta">
                 <div class="author">access4096</div>
-                <div class="date">2/20</div>
+                <div class="date">2025/02/24 16:54:15</div>
             </div>
         </div>
     </body>
@@ -33,11 +33,11 @@ def test_fetch_posts(mock_requests_get):
     posts = fetch_posts("NBA")
 
     assert len(posts) == 1  # 確保回傳 1 篇文章
-    assert posts[0]["board_id"] == "NBA"
+    assert posts[0]["board_name"] == "NBA"
     assert posts[0]["title"] == "[新聞] 林書豪6年來首踏NBA球場！為何曾拒絕參加"
-    assert posts[0]["author_name"] == "access4096"
+    assert posts[0]["author_id"] == "access4096"
     assert posts[0]["link"] == "https://www.ptt.cc/bbs/NBA/M.1740027377.A.CDD.html"
-    assert posts[0]["date"] == "2/20"
+    assert posts[0]["date"] == "2025/02/24 16:54:15"
 
 
 def test_fetch_posts_real():
@@ -45,9 +45,9 @@ def test_fetch_posts_real():
     posts = fetch_posts("NBA")
 
     assert requests.get("https://www.ptt.cc/bbs/NBA/index.html").status_code == 200
-    assert posts[0]['board_id'] == 'NBA'
+    assert posts[0]['board_name'] == 'NBA'
     assert "title" in posts[0]
-    assert "author_name" in posts[0]
+    assert "author_id" in posts[0]
     assert "date" in posts[0]
     assert "link" in posts[0]
     assert len(posts) > 1
